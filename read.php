48e54aae2bd747293cbfdc5126842190b315cdec
Alice Marique <mariquealice83@gmail.com>
2024-02-09 15:04:46 +0100

    .

--- read.php
+++ read.php
@@ -2,36 +2,6 @@
 require_once("./db.inc.php");
 $pdo = connect_db();
 
-// Vérifie si le formulaire a été soumis pour ajouter une randonnée
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['name'])) {
-    // Récupérer les données saisies dans le formulaire
-    $name = $_POST['name'];
-    $difficulty = $_POST['difficulty'];
-    $distance = $_POST['distance'];
-    $duration = $_POST['duration'];
-    $height = $_POST['height_difference'];
-
-    // Préparer la requête d'insertion
-    $sql = "INSERT INTO hiking (name, difficulty, distance, duration, height_difference) VALUES (:name, :difficulty, :distance, :duration, :height_difference)";
-    $stmt = $pdo->prepare($sql);
-
-    // Liaison des valeurs
-    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
-    $stmt->bindParam(':difficulty', $difficulty, PDO::PARAM_STR);
-    $stmt->bindParam(':distance', $distance, PDO::PARAM_INT);
-    $stmt->bindParam(':duration', $duration, PDO::PARAM_INT);
-    $stmt->bindParam(':height_difference', $height, PDO::PARAM_INT);
-
-    // Exécuter la requête d'insertion
-    if ($stmt->execute()) {
-        // Redirection vers read.php après l'ajout réussi
-        header("Location: read.php");
-        exit(); // Assure que le script se termine ici pour éviter toute exécution supplémentaire
-    } else {
-        echo "Erreur lors de l'ajout de la randonnée.";
-    }
-}
-
 ?>
 
 <!DOCTYPE html>
